8d97c819d1563509d9761815fb647bb86b296f17
Venz1001 <156085159+Venz1001@users.noreply.github.com>
2024-02-15 21:09:13 +0800

    Midterm gradeslip.c

--- gradeslip.c
+++ gradeslip.c
@@ -5,18 +5,17 @@ int main() {
     printf("\nNAME          : Venz Arlhey H. Glimada");
     printf("\nCOURSE        : BSINDTECH");
     printf("\n\n\n          :                FIRST SEMESTER 2023-2024");
-    printf("\n\n___________              __________________________________           ___________             __________:");
-    
-    printf("\n\n  SUBJECT                            DESCRIPTION                         UNITS                   M/G:");
-    printf("\n\n___________              __________________________________           ___________             __________:");
-    printf("\nGE CW 8                        The Contemporary World                        3                         ");
-    printf("\nLITE Elec                      Living in the IT Era                          3                         ");
-    printf("\nTech 326                       Programming                                   5                         ");
-    printf("\nIM 311B                        Technology Research 2                         2                         ");
-    printf("\nIM 323                          ");
-    printf("\nIM 324  ");
-    printf("\nFL 2                           Foreign Language 2                                                     ");
-    printf("\n\n___________              __________________________________           ___________             __________:");
-    printf("\n\n                                    TOTAL UNITS                           22                             ");
+    printf("\n\n___________      __________________________________________________                 ____________             _________ ");
+    printf("\n\n  SUBJECT        DESCRIPTION                                                           UNITS                   M/G:");
+    printf("\n\n___________      __________________________________________________                 ____________             _________ ");
+    printf("\nGE CW 8            The Contemporary World                                                  3                             ");
+    printf("\nLITE Elec          Living in the IT Era                                                    3                             ");
+    printf("\nTech 326           Programming                                                             5                             ");
+    printf("\nIM 311B            Technology Research 2                                                   2                             ");
+    printf("\nIM 323             Industrial Organization and Personal Management                         3                             ");
+    printf("\nIM 324             Shop Supervision and Apprenticeship Training                            3                             ");
+    printf("\nFL 2               Foreign Language 2                                                      3                             ");
+    printf("\n\n___________      __________________________________________________                 ____________             _________ ");
+    printf("\n\n                                    TOTAL UNITS                                          22                            ");
     return 0;
 }
